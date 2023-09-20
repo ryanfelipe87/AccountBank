867ce4a24cadde56c7fda644f9a38ce624e881b4
Ryan <ryanfelipe.rr@gmail.com>
2023-09-20 14:45:34 -0300

    Initial commit

---
+++ TerminalAccount.java
@@ -0,0 +1,12 @@
+public class TerminalAccount {
+
+    public static void main(String[] args){
+
+        String nameClient;
+        String agency;
+        int numberAccount;
+        Double balance;
+
+
+    }
+}
